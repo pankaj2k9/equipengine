ca178289c8dd97c09656ad54727beecb605af736
denniscual <mamaylovepapay@outlook.com>
2017-10-10 09:07:31 +0800

    adding vs-code debugger and supporting the css pre-processors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './base_components/Navbar';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Navbar />
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
-        );
-    }
+  render() {
+    let a;
+    return (
+      <div className="App">
+        <Navbar />
+        {(a = 5)}
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
-
 
 export default App;
