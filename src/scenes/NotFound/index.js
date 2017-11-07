6695758cbf7590bb8f1ee90cf2ed737cc67d2f0e
denniscual <mamaylovepapay@outlook.com>
2017-11-07 19:36:15 +0800

    removing the margin top to some components, we will leave it to the main component

--- src/scenes/NotFound/index.js
+++ src/scenes/NotFound/index.js
@@ -1,44 +1,23 @@
 import React from 'react';
 
 import Box from 'base_components/Box';
-import Media from 'react-media';
 
 const NotFound = () => {
   return (
-    <Media query="(max-width: 767px)">
-      {matches =>
-        matches ? (
-          <Box>
-            <h4>Page Not Found</h4>
-            <p>
-              Praesent sapien massa, convallis a pellentesque nec, egestas non
-              nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac
-              lectus. Curabitur non nulla sit amet nisl tempus convallis quis ac
-              lectus. Vestibulum ante ipsum primis in faucibus orci luctus et
-              ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet
-              aliquam vel, ullamcorper sit amet ligula. Vivamus magna justo,
-              lacinia eget consectetur sed, convallis at tellus. Curabitur non
-              nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque
-              in ipsum id orci porta dapibus.
-            </p>
-          </Box>
-        ) : (
-          <Box style={{ marginTop: 145 }}>
-            <h4>Page Not Found</h4>
-            <p>
-              Praesent sapien massa, convallis a pellentesque nec, egestas non
-              nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac
-              lectus. Curabitur non nulla sit amet nisl tempus convallis quis ac
-              lectus. Vestibulum ante ipsum primis in faucibus orci luctus et
-              ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet
-              aliquam vel, ullamcorper sit amet ligula. Vivamus magna justo,
-              lacinia eget consectetur sed, convallis at tellus. Curabitur non
-              nulla sit amet nisl tempus convallis quis ac lectus. Pellentesque
-              in ipsum id orci porta dapibus.
-            </p>
-          </Box>
-        )}
-    </Media>
+    <Box>
+      <h4>Page Not Found</h4>
+      <p>
+        Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+        Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia Curae; Donec velit neque, auctor sit amet aliquam vel,
+        ullamcorper sit amet ligula. Vivamus magna justo, lacinia eget
+        consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl
+        tempus convallis quis ac lectus. Pellentesque in ipsum id orci porta
+        dapibus.
+      </p>
+    </Box>
   );
 };
 
