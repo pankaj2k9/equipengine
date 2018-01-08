010aa15b97b2a8f8c117f75e6c6312cd64bba031
bunso <denniscual0716@gmail.com>
2018-01-08 20:52:00 +0800

    finishing the FormOtherInfo component

--- src/scenes/Admin/AdminUserManager/components/FormAdminUser/components/FormOtherInfo/index.js
+++ src/scenes/Admin/AdminUserManager/components/FormAdminUser/components/FormOtherInfo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MenuItem } from 'react-bootstrap';
 // components
 import Form, {
   FormGroup,
@@ -11,8 +12,9 @@ import FormGroupLeft from '../FormGroupLeft';
 import ButtonAdd from 'base_components/ButtonAdd';
 import PanelAdminUser from '../PanelAdminUser';
 import ContainerFlex from 'base_components/ContainerFlex';
+import Dropdown from 'base_components/Dropdown';
 
-// overriding styles for the button add
+// button add group
 const ButtonAddGroup = styled(ButtonAdd)`
   border: 1px solid #979797;
   background: transparent;
@@ -25,10 +27,33 @@ const ButtonAddGroup = styled(ButtonAdd)`
   }
 `;
 
-// const overriding the Form
+// form add group
 const FormAddGroup = styled(Form)`
   display: flex;
   justify-content: flex-end;
+`;
+
+// form group downdown
+const FormGroupDropdown = styled(FormGroup)`
+  flex: 1;
+  margin-right: 0.6em;
+
+  .dropdown {
+    width: 100%;
+
+    button {
+      width: 100%;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
+`;
+
+// for zip code text
+const FormGroupZipCode = styled(FormGroup)`
+  width: 25%;
 `;
 
 const FormOtherInfo = () => (
@@ -47,6 +72,29 @@ const FormOtherInfo = () => (
       <Label>Address</Label>
       <TextArea name="address" row={4} placeholder="Address" />
     </FormGroup>
+    <ContainerFlex>
+      <FormGroupDropdown>
+        <Dropdown title="Country" id="dropdown-size-medium">
+          <MenuItem eventKey="1">Action</MenuItem>
+          <MenuItem eventKey="2">Another action</MenuItem>
+          <MenuItem eventKey="3">Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey="4">Separated link</MenuItem>
+        </Dropdown>
+      </FormGroupDropdown>
+      <FormGroupDropdown>
+        <Dropdown title="State" id="dropdown-size-medium">
+          <MenuItem eventKey="1">Action</MenuItem>
+          <MenuItem eventKey="2">Another action</MenuItem>
+          <MenuItem eventKey="3">Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey="4">Separated link</MenuItem>
+        </Dropdown>
+      </FormGroupDropdown>
+      <FormGroupZipCode>
+        <Text name="zipCode" placeholder="Zip code" />
+      </FormGroupZipCode>
+    </ContainerFlex>
     <FormAddGroup>
       <ButtonAddGroup text="Add Group" />
     </FormAddGroup>
