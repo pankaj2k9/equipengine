f2b0003b9049122ff412f11bc65b3489ce942dea
bunso <denniscual0716@gmail.com>
2018-01-26 10:14:46 +0800

    moving the loggedUser reducer into login scene/feature

--- src/data/users/reducer.js
+++ src/data/users/reducer.js
@@ -1,31 +1,28 @@
-import { identical, isNil } from 'ramda';
-import { GET_LOGGED_USER, GET_USERS } from './actionTypes';
-import { combineReducers } from 'redux';
+import { GET_USERS } from './actionTypes';
 
-// initial users for our app.
-const initialUsers = [
-  {
+const initialUsers = {
+  '23423423424jj324': {
     _id: '23423423424jj324',
     name: 'Irish Jane',
     type: 'Admin',
     username: 'irishjane',
     password: '123456'
   },
-  {
+  '234238sdjf34234j': {
     _id: '234238sdjf34234j',
     name: 'Drish Cual',
     type: 'Student',
     username: 'drish',
     password: '123456'
   },
-  {
+  '234234nj23lkj423': {
     _id: '234234nj23lkj423',
     name: 'Dennis Martin',
     type: 'Teacher',
     username: 'drish',
     password: '123456'
   }
-];
+};
 
 // reducers for managing users
 const users = (state = initialUsers, action) => {
@@ -39,39 +36,4 @@ const users = (state = initialUsers, action) => {
   }
 };
 
-// getting user by username and password
-const getUserByAuth = ({ users, username, password }) =>
-  users.find(
-    user =>
-      identical(user.username, username) && identical(user.password, password)
-  );
-
-/**
- * loggedUser :: (state, action) -> state
- *
- * Managing the state of loggedUser
- */
-const loggedUser = (state = {}, action) => {
-  switch (action.type) {
-    case GET_LOGGED_USER: {
-      const { username, password } = action.payload;
-      // get logged user
-      const loggedUser = getUserByAuth({
-        users: initialUsers, // we will pass default users
-        username,
-        password
-      });
-
-      // if loggedUser is undefined we will return empty object else return loggedUser
-      return !isNil(loggedUser) ? loggedUser : {};
-    }
-    default: {
-      return state;
-    }
-  }
-};
-
-export default combineReducers({
-  users,
-  loggedUser
-});
+export default users;
--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-// get reducer on data module user
+// get reducer
 import { reducer as users } from './data/users';
+import loggedUser from './scenes/Login/reducer';
 
 // this is the root reducers, we will combine all reducers that we are using.
 export default combineReducers({
   routing: routerReducer,
-  users
+  users,
+  loggedUser
 });
