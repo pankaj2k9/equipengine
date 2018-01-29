5e107c1f3f97aeae77d697f5f5ae15033aaf6dd3
bunso <denniscual0716@gmail.com>
2018-01-29 19:33:34 +0800

    create InitialRoutes component that hides the conditional statement for rendering either public or private routes

---
+++ src/scenes/Root/components/App/InitRoutes/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+import { identical, isEmpty } from 'ramda';
+
+// base_components
+import Main from 'base_components/Main';
+
+/* for code splitting - loadable */
+import Loadable from 'react-loadable';
+import Loader from 'base_components/Loader';
+
+// import all routes
+import PublicRoutes from '../PublicRoutes';
+import PrivateRoutes from '../PrivateRoutes';
+import TeacherRoutes from '../TeacherRoutes';
+import AdminRoutes from '../AdminRoutes';
+
+/* create our async component in here
+ * this will optimize the loading of components of our app
+ * dynamically importing our scenes here*/
+const AsyncMainBars = Loadable({
+  loader: () => import('base_components/SidebarTransition'),
+  loading: () => null, // before this component gets loaded, we will render first this Loader component.
+  timeout: 10000
+});
+
+const AsyncNavbarAdmin = Loadable({
+  loader: () => import('base_components/NavbarAdmin'),
+  loading: Loader,
+  timeout: 10000
+});
+
+const AsyncNotFound = Loadable({
+  loader: () => import('scenes/NotFound'),
+  loading: Loader,
+  timeout: 10000
+});
+
+/**
+ * InitRoutes component will render either Public or Private routes based on the type.
+ * If the type is empty, we need to render public. Else render private.
+ */
+const InitRoutes = ({ type }) => (
+  <div>
+    {isEmpty(type) ? (
+      <PublicRoutes />
+    ) : (
+      <div>
+        {/*-------------------------- PRIVATE COMPONENTS ---------------------*/}
+        <Route
+          path="/"
+          render={() =>
+            // conditional rendering.
+            identical(type, 'Student') || identical(type, 'Teacher') ? (
+              <AsyncMainBars />
+            ) : (
+              <AsyncNavbarAdmin />
+            )}
+        />
+        <Switch>
+          <Main type={type}>
+            <PrivateRoutes />
+            <TeacherRoutes />
+            <AdminRoutes />
+            {/* no routes match */}
+            <Route component={AsyncNotFound} />
+          </Main>
+        </Switch>
+      </div>
+    )}
+  </div>
+);
+
+InitRoutes.propTypes = {
+  type: PropTypes.string
+};
+
+export default InitRoutes;
--- src/scenes/Root/components/App/index.js
+++ src/scenes/Root/components/App/index.js
@@ -1,70 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route } from 'react-router-dom';
-import { identical } from 'ramda';
+//
+import InitRoutes from './InitRoutes';
 
-// base_components
-import Main from 'base_components/Main';
-import ErrorBoundary from 'base_components/ErrorBoundary';
-
-/* for code splitting - loadable */
-import Loadable from 'react-loadable';
-import Loader from 'base_components/Loader';
-
-// import all routes
-import PublicRoutes from './PublicRoutes';
-import PrivateRoutes from './PrivateRoutes';
-import TeacherRoutes from './TeacherRoutes';
-import AdminRoutes from './AdminRoutes';
-
-/* create our async component in here
- * this will optimize the loading of components of our app
- * dynamically importing our scenes here*/
-const AsyncMainBars = Loadable({
-  loader: () => import('base_components/SidebarTransition'),
-  loading: () => null, // before this component gets loaded, we will render first this Loader component.
-  timeout: 10000
-});
-
-const AsyncNavbarAdmin = Loadable({
-  loader: () => import('base_components/NavbarAdmin'),
-  loading: Loader,
-  timeout: 10000
-});
-
-const AsyncNotFound = Loadable({
-  loader: () => import('scenes/NotFound'),
-  loading: Loader,
-  timeout: 10000
-});
-
-// FIXME: Fix the not found page.
-const App = ({ loggedUser: { type } }) => {
-  return (
-    <div>
-      {/*-------------------------- PRIVATE COMPONENTS ---------------------*/}
-      <Route
-        path="/"
-        render={() =>
-          // conditional rendering.
-          identical(type, 'Student') || identical(type, 'Teacher') ? (
-            <AsyncMainBars />
-          ) : (
-            <AsyncNavbarAdmin />
-          )}
-      />
-      <Switch>
-        <Main type={type}>
-          <PrivateRoutes />
-          <TeacherRoutes />
-          <AdminRoutes />
-          {/* no routes match */}
-          <Route component={AsyncNotFound} />
-        </Main>
-      </Switch>
-    </div>
-  );
-};
+// App component will render all components which are associated to the URL.
+const App = ({ loggedUser: { type } }) => <InitRoutes type={type} />;
 
 App.propTypes = {
   loggedUser: PropTypes.object
