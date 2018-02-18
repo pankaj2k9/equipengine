6afddb7d231a22e71ed3d5c13120cf05476a41cb
Dennis Cual <mamaylovepapay@outlook.com>
2018-02-18 22:43:56 +0800

    finished the BodyBrowseGroups and finalizing the BrowseGroups scene css and js at Desktop viewport

--- src/scenes/BrowseGroups/components/BodyBrowseGroups/components/ListLatestNotification/index.js
+++ src/scenes/BrowseGroups/components/BodyBrowseGroups/components/ListLatestNotification/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 //
 import DateTime from 'base_components/DateTime';
+import ContainerFlex from 'base_components/ContainerFlex';
 
 const ListItem = styled(({ className }) => (
   <li className={className}>
-    <DateTime date="22 Sep" time="2:22am" />
-    <p>
-      <b>John Mcall</b> commented on{' '}
-      <em>Tutorial 1.1: Communication and Culture</em>
-    </p>
+    <ContainerFlex isSpaceBetween isAlignCenter>
+      <DateTime date="22 Sep" time="2:22am" />
+      <p>
+        <b>John Mcall</b> commented on{' '}
+        <strong>Tutorial 1.1: Communication and Culture</strong>
+      </p>
+    </ContainerFlex>
   </li>
-))``;
+))`
+  margin-bottom: 1em;
+
+  > div > div {
+    margin-right: 1.8em;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+`;
 
 const HeaderListLatest = styled.header`
   border-bottom: 1px solid #dadada;
@@ -20,13 +33,17 @@ const HeaderListLatest = styled.header`
   color: #000;
 `;
 
+const ListContainer = styled.ul`
+  padding: 1em 0;
+`;
+
 const ListLatestNotification = () => (
   <div>
     <HeaderListLatest>Latest Notification</HeaderListLatest>
-    <ul>
+    <ListContainer>
       <ListItem />
       <ListItem />
-    </ul>
+    </ListContainer>
   </div>
 );
 
