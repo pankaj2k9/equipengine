9211fb9fdc43b6aee68de24de4bd62a833f42055
Dennis Cual <mamaylovepapay@outlook.com>
2018-02-22 23:03:35 +0800

    add performance optimization to our drag and drop component

--- src/scenes/Root/components/App/Sample.js
+++ src/scenes/Root/components/App/Sample.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { List } from 'immutable';
-import { not, isNil } from 'ramda';
+import { not, isNil, identical } from 'ramda';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // fake data generator
@@ -16,6 +16,48 @@ const reorderItems = (list, startIndex, endIndex) =>
     .delete(startIndex) // delete element to the position of given startIndex then return the new array which excludes the deleted element.
     .insert(endIndex, List(list).get(startIndex)) // insert the element from startIndex to the position of endIndex.
     .toArray(); // return an array instead of List
+
+class Inner extends Component {
+  shouldComponentUpdate(nextProps) {
+    if (identical(this.props.items, nextProps.items)) {
+      console.log('equal');
+      return false;
+    }
+    console.log('not equal');
+    return true;
+    /* return identical(this.props.items, nextProps.items) ? false : true;*/
+  }
+
+  render() {
+    return this.props.items.map(({ id, content }, i) => (
+      <Draggable draggableId={id} type="PERSON" index={i} key={id}>
+        {(provided, snapshot) => {
+          // extending the DraggableStyle with our own inline styles
+          const style = {
+            backgroundColor: snapshot.isDragging ? 'green' : 'white',
+            fontSize: 18,
+            padding: 2,
+            margin: '0 0 2px 0',
+            ...provided.draggableProps.style
+          };
+          return (
+            <div>
+              <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                style={style}
+              >
+                <span>{content}</span>
+              </div>
+              {provided.placeholder}
+            </div>
+          );
+        }}
+      </Draggable>
+    ));
+  }
+}
 
 class Sample extends Component {
   state = {
@@ -39,34 +81,11 @@ class Sample extends Component {
               ref={provided.innerRef}
               style={{
                 backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey',
-                width: '50%'
+                width: '50%',
+                padding: 8
               }}
             >
-              {this.state.items.map(({ id, content }, i) => (
-                <Draggable draggableId={id} type="PERSON" index={i} key={id}>
-                  {(provided, snapshot) => {
-                    // extending the DraggableStyle with our own inline styles
-                    const style = {
-                      backgroundColor: snapshot.isDragging ? 'green' : 'white',
-                      fontSize: 18,
-                      ...provided.draggableProps.style
-                    };
-                    return (
-                      <div>
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={style}
-                        >
-                          <span>{content}</span>
-                        </div>
-                        {provided.placeholder}
-                      </div>
-                    );
-                  }}
-                </Draggable>
-              ))}
+              <Inner items={this.state.items} />
               {provided.placeholder}
             </div>
           )}
