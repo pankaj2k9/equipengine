9a795d484e1be8b1a3bacb9b2b514e8869dedc74
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-22 15:10:13 +0800

    extract propTypes, defaultProps and styles in ListObjectives

--- src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionObjectiveCourse/components/ListObjectives/index.js
+++ src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionObjectiveCourse/components/ListObjectives/index.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react'
-import styled from 'styled-components'
 //
 import ContainerFlex from 'base_components/ContainerFlex'
 import Dropdown from 'base_components/Dropdown'
@@ -9,49 +8,48 @@ import Spinner from 'base_components/Spinner'
 import IconMenu from 'react-icons/lib/md/dehaze'
 import IconClose from 'react-icons/lib/md/close'
 import DragDrop, { DroppableList, DroppableListItem } from 'base_components/DragDrop'
-
-const ContainerButton = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-right: 2.4em;
-  text-align: center;
-
-  label {
-    color: #727272;
-    font-size: 0.7rem;
-    margin-top: 1em;
-  }
-`
+// assets
+import listObjectivesStyles, { ContainerAction } from './styles'
 
 const DropdownResult = Dropdown.extend`
   width: 100%;
 `
 
-const List = styled(DroppableList)`
-  border: 1px solid #d8d8d8;
-  border-bottom: 0;
-  overflow: auto;
-`
-
-const ListObjectives = () => (
+/**
+ * -------------------------------------
+ * List
+ * @see ListObjectives
+ * -------------------------------------
+ */
+const ListObjectives = listObjectivesStyles(({className}) => (
   <DragDrop>
-    <List droppableId='123'>
+    <DroppableList droppableId='123' className={className}>
       {
         () => (
           <Fragment>
-            <ListItem draggableId='1' index={0} />
-            <ListItem draggableId='2' index={1} />
+            <DroppableListItem draggableId='1' index={0}>
+              <ListItemObjective />
+            </DroppableListItem>
+            <DroppableListItem draggableId='2' index={1}>
+              <ListItemObjective />
+            </DroppableListItem>
           </Fragment>
         )
       }
-    </List>
+    </DroppableList>
   </DragDrop>
-)
+))
 
 export default ListObjectives
 
-const ListItem = styled(({ className, draggableId, index }) => (
-  <DroppableListItem draggableId={draggableId} index={index} className={className}>
+/**
+ * -------------------------------------
+ * ListItemObjective
+ * @see ListObjectives
+ * -------------------------------------
+ */
+const ListItemObjective = () => (
+  <Fragment>
     <ContainerFlex isSpaceBetween>
       <ContainerFlex isAlignCenter className='ListItem__title'>
         <i>
@@ -60,7 +58,7 @@ const ListItem = styled(({ className, draggableId, index }) => (
         <span>Complete all the lessons</span>
       </ContainerFlex>
       <ContainerFlex isAlignCenter className='ListItem__action'>
-        <ContainerButton>
+        <ContainerAction>
           <DropdownResult title='Pass/Fail' id='dropdown-size-medium'>
             <MenuItem eventKey='1'>Percentage</MenuItem>
             <MenuItem eventKey='2'>Pass/Fail</MenuItem>
@@ -68,32 +66,15 @@ const ListItem = styled(({ className, draggableId, index }) => (
             <MenuItem eventKey='4'>Manual Entry</MenuItem>
           </DropdownResult>
           <Label>Assesment Method</Label>
-        </ContainerButton>
-        <ContainerButton>
+        </ContainerAction>
+        <ContainerAction>
           <Spinner />
           <Label>Weight of Mark</Label>
-        </ContainerButton>
+        </ContainerAction>
         <i>
           <IconClose />
         </i>
       </ContainerFlex>
     </ContainerFlex>
-  </DroppableListItem>
-))`
-  padding: 1.2em 0.8em 1.2em;
-  border-bottom: 1px solid #d8d8d8;
-
-  .ListItem__title {
-    i {
-      color: #d8d8d8;
-      margin-right: 1.2em;
-    }
-  }
-
-  .ListItem__action {
-    > i {
-      color: #afafaf;
-      font-size: 1.4rem;
-    }
-  }
-`
+  </Fragment>
+)
---
+++ src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionObjectiveCourse/components/ListObjectives/styles.js
@@ -0,0 +1,52 @@
+import css, { extendCss } from 'utils/addStyles'
+import ContainerFlex from 'base_components/ContainerFlex'
+
+/**
+ * -------------------------------------
+ * Styles for ListObjectives
+ * @function
+ * -------------------------------------
+ */
+export default css(`
+  border: 1px solid #d8d8d8;
+  border-bottom: 0;
+  overflow: auto;
+
+  li {
+    padding: 1.2em 0.8em 1.2em;
+    border-bottom: 1px solid #d8d8d8;
+
+    .ListItem__title {
+      i {
+        color: #d8d8d8;
+        margin-right: 1.2em;
+      }
+    }
+
+    .ListItem__action {
+      > i {
+        color: #afafaf;
+        font-size: 1.4rem;
+      }
+    }
+  }
+`)
+
+/**
+ * -------------------------------------
+ * ContainerAction styled components
+ * @function
+ * -------------------------------------
+ */
+export const ContainerAction = extendCss(`
+  display: flex;
+  flex-direction: column;
+  margin-right: 2.4em;
+  text-align: center;
+
+  label {
+    color: #727272;
+    font-size: 0.7rem;
+    margin-top: 1em;
+  }
+`)(ContainerFlex)
