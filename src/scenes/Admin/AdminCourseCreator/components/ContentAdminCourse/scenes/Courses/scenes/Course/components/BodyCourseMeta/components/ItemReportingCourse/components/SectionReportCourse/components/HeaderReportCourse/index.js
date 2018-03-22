fb942d545852a81d5fe91e25203d15b1a6eead15
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-22 13:57:15 +0800

    update the HeaderReportCourse. Extract the styles and separate into styles.js

--- src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionReportCourse/components/HeaderReportCourse/index.js
+++ src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionReportCourse/components/HeaderReportCourse/index.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import styled from 'styled-components'
 //
 import TitleSection from '../../../TitleSection'
-import Dropdown from 'base_components/RootDropdown'
-import css from 'utils/addStyles'
+import ContainerFlex from 'base_components/ContainerFlex'
+// assets
+import headerReportCourseStyles, { DropdownResult } from './styles'
 
-// TODO: Extract the propTypes and styles to other files.
-const DropdownResult = css(`
-  width: 160px;
-`)(Dropdown)
-
-const HeaderReportCourse = styled(({ className }) => (
+const HeaderReportCourse = headerReportCourseStyles(({ className }) => (
   <header className={className}>
     <TitleSection>Report</TitleSection>
-    <div>
+    <ContainerFlex isAlignCenter>
       <span>Final Mark Result</span>
       <DropdownResult
         dropdownName='markResult'
@@ -24,15 +19,8 @@ const HeaderReportCourse = styled(({ className }) => (
           { label: 'Manual Entry', value: 'manualEntry' }
         ]}
       />
-    </div>
+    </ContainerFlex>
   </header>
-))`
-  padding-bottom: 0.8em;
-  border-bottom: 1px solid #d8d8d8;
-
-  > div > span:first-child {
-    margin-right: 2em;
-  }
-`
+))
 
 export default HeaderReportCourse
---
+++ src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/BodyCourseMeta/components/ItemReportingCourse/components/SectionReportCourse/components/HeaderReportCourse/styles.js
@@ -0,0 +1,30 @@
+import css from 'utils/addStyles'
+import Dropdown from 'base_components/RootDropdown'
+
+/**
+ * -------------------------------------
+ * Styles for HeaderReportCourse
+ * @function
+ * -------------------------------------
+ */
+export default css(`
+  padding-bottom: 0.8em;
+  border-bottom: 1px solid #d8d8d8;
+
+  > div {
+    margin-bottom: 0.5em;
+
+    > span:first-child {
+      margin-right: 2em;
+    }
+  }
+`
+)
+
+/**
+ * -------------------------------------
+ * Styles for HeaderReportCourse
+ * @function
+ * -------------------------------------
+ */
+export const DropdownResult = css('width: 160px;')(Dropdown)
