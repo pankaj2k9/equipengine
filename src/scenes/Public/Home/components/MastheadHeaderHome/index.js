3826f1b1b0bdf856f124ec63c2eaadef45013767
Dennis Cual <mamaylovepapay@outlook.com>
2018-03-27 13:49:18 +0800

    extract styles in MastheadHeader component

--- src/scenes/Public/Home/components/MastheadHeaderHome/index.js
+++ src/scenes/Public/Home/components/MastheadHeaderHome/index.js
@@ -1,47 +1,8 @@
 import React from 'react'
-import css from 'utils/css'
 // assets
 import LinkButton from 'base_components/LinkButton'
-import { defaultFlexStyles } from 'base_components/ContainerFlex'
 import bannerImg from '../../banner.svg'
-
-const mastheadHomeStyles = css`
-  h2, img {
-    max-width: 720px;
-    width: 100%;
-  }
-
-  h2 {
-    color: #ffffff;
-    font-family: "Avenir Next", sans-serif;
-    font-weight: 800;
-    font-size: 1.6rem;
-    text-align: center;
-    margin-top: 1em;
-  }
-
-  img {
-    object-fit: cover;
-    margin-top: 1.7em;
-  }
-
-  @media screen and (min-width: 768px) {
-    h2 {
-      font-size: 2rem;
-      margin-top: 4em;
-      line-height: 1.4;
-    }
-
-    a {
-      padding: 0.9em 0;
-      font-size: 1.1rem;
-      width: 173px;
-    }
-  }
-`
-
-// create section using defaultFlexStyles
-const SectionFlex = defaultFlexStyles('section')
+import mastheadHomeStyles, { SectionFlex } from './styles'
 
 const MastheadHome = mastheadHomeStyles(({className}) => (
   <SectionFlex
---
+++ src/scenes/Public/Home/components/MastheadHeaderHome/styles.js
@@ -0,0 +1,51 @@
+import css from 'utils/css'
+import { defaultFlexStyles } from 'base_components/ContainerFlex'
+
+/**
+ * -------------------------------------
+ * Styles for MastheadHeaderHome
+ * @function
+ * -------------------------------------
+ */
+export default css`
+  h2, img {
+    max-width: 720px;
+    width: 100%;
+  }
+
+  h2 {
+    color: #ffffff;
+    font-family: "Avenir Next", sans-serif;
+    font-weight: 800;
+    font-size: 1.6rem;
+    text-align: center;
+    margin-top: 1em;
+  }
+
+  img {
+    object-fit: cover;
+    margin-top: 1.7em;
+  }
+
+  @media screen and (min-width: 768px) {
+    h2 {
+      font-size: 2rem;
+      margin-top: 4em;
+      line-height: 1.4;
+    }
+
+    a {
+      padding: 0.9em 0;
+      font-size: 1.1rem;
+      width: 173px;
+    }
+  }
+`
+
+/**
+ * -------------------------------------
+ * SectionFlex styled component
+ * @function
+ * -------------------------------------
+ */
+export const SectionFlex = defaultFlexStyles('section')
