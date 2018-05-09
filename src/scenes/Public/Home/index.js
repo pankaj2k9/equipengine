6d56a839aa238aeec1da17b63b04abe87c560d1c
Dennis Cual <mamaylovepapay@outlook.com>
2018-05-09 15:47:23 +0800

    udpate the repo

--- src/scenes/Public/Home/index.js
+++ src/scenes/Public/Home/index.js
@@ -1,23 +1,9 @@
 import React from 'react'
+import { createProvider, consume } from 'storext'
 // assets
 import MastheadHeaderHome from './components/MastheadHeaderHome'
 import NavbarHeaderHome from './components/NavbarHeaderHome'
 import homeStyles, { headerHomeStyles, mainHomeStyles } from './styles'
-
-/**
- * -------------------------------------
- * Home
- * @see HeaderHome
- * -------------------------------------
- */
-const Home = homeStyles(({className}) => (
-  <div className={className}>
-    <HeaderHome />
-    <MainHome />
-  </div>
-))
-
-export default Home
 
 /**
  * -------------------------------------
@@ -45,3 +31,67 @@ const MainHome = mainHomeStyles(({className}) => (
     </section>
   </main>
 ))
+
+// creating Counter store
+const counterStore = {
+  state: {
+    count: 0
+  },
+  updaters: {
+    increment ({count}, state) {
+      return state.update('count', (value) => value + count)
+    },
+    decrement ({count}, state) {
+      return state.update('count', (value) => value - count)
+    }
+  }
+}
+
+const { Provider } = createProvider({counter: counterStore}, {isLoggerOn: true})
+
+const Counter = ({count, handlers}) => (
+  <div>
+    <div><span>{count}</span></div>
+    <button onClick={handlers.decrement}>decrement</button>
+    <button onClick={handlers.increment}>increment</button>
+  </div>
+)
+
+const mapState = (state) => ({
+  count: state.counter.get('count')
+})
+
+const mapHandlers = (commit) => ({
+  increment () {
+    commit({
+      type: 'increment',
+      payload: {count: 10}
+    })
+  },
+  decrement () {
+    commit({
+      type: 'decrement',
+      payload: {count: 10}
+    })
+  }
+})
+
+const ContainerCounter = consume(mapState, mapHandlers)(Counter)
+
+/**
+ * -------------------------------------
+ * Home
+ * @see HeaderHome
+ * -------------------------------------
+ */
+const Home = homeStyles(({className}) => (
+  <Provider>
+    <div className={className}>
+      <HeaderHome />
+      <MainHome />
+      <ContainerCounter />
+    </div>
+  </Provider>
+))
+
+export default Home
--- src/utils/context/lib/utils.js
+++
@@ -1,6 +0,0 @@
-import { compose, not, equals } from 'ramda'
-// Predicate function for evaluating if the 2 values is not equal.
-export const isNotEqual = compose(not, equals)
-
-// Get the display name of a function/react which is passed.
-export const getDisplayName = (WrappedComponent) => WrappedComponent.displayName || WrappedComponent.name || 'Component'
