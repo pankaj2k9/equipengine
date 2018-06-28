d3f5078193f694caf7b959f7dfa4fd44229ad029
Dmitry Rog <dimanius851@gmail.com>
2018-06-28 17:53:21 +0400

    Small fix for firefox about aligning LogoContainer in NavBar - Also moved LogoContainer to styled-components

--- src/base_components/LogoContainer/index.js
+++ src/base_components/LogoContainer/index.js
@@ -1,17 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-import './styles.css'
 import Logo from 'resources/images/logo.svg'
 import { $textBoldColor } from 'shared/color.js'
 
+const Img = styled.img`
+  margin-right: 0.5em;
+  height: 31px;
+
+  @media screen and (min-width: 768px) {
+    margin-right: 1em;
+    height: 100%;
+  }
+`
+
+const H1 = styled.h1`
+  font-family: 'Avenir Next', sans-serif;
+  font-weight: 800;
+  font-size: 18px;
+  margin-bottom: 0;
+  color: ${$textBoldColor};
+
+  @media screen and (min-width: 768px) {
+    font-size: 22px;
+  }
+`
+
 const LogoContainer = () => {
   return (
-    <Link className='LogoContainer' to='/secure/browse-groups'>
-      <img alt='Logo' src={Logo} />
-      <h1 style={{ color: $textBoldColor }}>EquipEngine</h1>
+    <Link to='/secure/browse-groups'>
+      <Img alt='Logo' src={Logo} />
+      <H1 style={{ color: $textBoldColor }}>EquipEngine</H1>
     </Link>
   )
 }
 
-export default LogoContainer
+export default styled(LogoContainer)`
+  display: flex;
+  align-items: center;
+  padding: 5% 5% 4% 5%;
+  background-color: #fff;
+  min-height: 59px;
+
+  @media screen and (min-width: 768px) {
+    padding: 9px 35px 9px 20px;
+    margin-bottom: 0px;
+  }
+`
