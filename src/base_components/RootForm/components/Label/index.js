abf94b4dc7712574a4b0973fe3b02c101cbd5f6a
Dmitry Rog <dimanius851@gmail.com>
2018-06-28 21:01:51 +0400

    Changed Account Role container - Remove the "Exclude students from teacher" fields with the drop-down - Add the new design in "Account Role" section - Added WhireArea base component for future using - Some refactoring in affected components

--- src/base_components/RootForm/components/Label/index.js
+++ src/base_components/RootForm/components/Label/index.js
@@ -1,7 +1,18 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const Label = styled.label`
+const Label = styled(({
+  className,
+  htmlFor,
+  children
+}) => (
+  <label
+    htmlFor={htmlFor}
+    className={className}>
+    {children}
+  </label>
+))`
   color: ${(props) => (props.light ? '#777777' : '#000000')};
   font-weight: 400;
 `
---
+++ src/base_components/WhiteArea/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const WhiteArea = styled.div`
+  border: 2px solid #cccccc;
+  padding: 14px;
+  background-color: #ffffff;
+  min-height: 141px;
+  margin-bottom: 30px;
+`
+
+export default WhiteArea
