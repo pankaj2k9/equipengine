f07e9852e14ca32183f1b462747092190caadade
Dmitry Rog <dimanius851@gmail.com>
2018-07-01 18:39:14 +0400

    Styling

---
+++ src/scenes/Admin/AdminCourseCreator/components/ContentAdminCourse/scenes/Courses/scenes/Course/components/HeaderCourseMeta/components/ButtonsContainer/index.js
@@ -0,0 +1,9 @@
+import ContainerFlex from "base_components/ContainerFlex"
+
+const ButtonsContainer = ContainerFlex.extend`
+  button:last-child {
+    margin-left: 14px;
+  }
+`
+
+export default ButtonsContainer
