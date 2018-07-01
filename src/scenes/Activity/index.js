f22def5056af041a7b02d2fccb27c89b9e7f56b3
Alex Soroka <lehaSVV2009@gmail.com>
2018-07-02 01:40:55 +0300

    Move mock data to separate json files

--- src/scenes/Activity/index.js
+++ src/scenes/Activity/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import IconActivity from "react-icons/lib/fa/line-chart"
 import Loading from "base_components/Loading"
 //
+import activitiesData from "./activities.json"
 import ActionActivities from "./components/ActionActivities"
 import ListActivities from "./components/ListActivities"
 import PageWrapper from "../components/PageWrapper"
@@ -18,60 +19,7 @@ class Activity extends Component {
       () =>
         this.setState({
           fetchingActivities: false,
-          activities: [
-            {
-              id: 1,
-              eventable_type: "Comment",
-              flagged: false,
-              status: "pending",
-              user: {
-                id: 5,
-                first_name: "Jon",
-                last_name: "Mcall",
-                avatar: {
-                  url:
-                    "https://s3.amazonaws.com/uifaces/faces/twitter/ripplemdk/128.jpg"
-                }
-              },
-              course: {
-                id: 1,
-                title: "Communication and Culture"
-              },
-              lesson: {
-                title: "Tutorial 1.1"
-              },
-              created_at: {
-                date: "22 Sep",
-                time: "1:00pm"
-              }
-            },
-            {
-              id: 2,
-              eventable_type: "Comment",
-              flagged: true,
-              status: "not-pending",
-              user: {
-                id: 6,
-                first_name: "Jill",
-                last_name: "Mane",
-                avatar: {
-                  url:
-                    "https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg"
-                }
-              },
-              course: {
-                id: 2,
-                title: "Becoming Equipment to Communicate"
-              },
-              lesson: {
-                title: "Tutorial 1.3"
-              },
-              created_at: {
-                date: "22 Sep",
-                time: "1:00pm"
-              }
-            }
-          ]
+          activities: activitiesData
         }),
       2000
     )
