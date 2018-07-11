8e5596de20c33c3b4419ed080026c6b24629556b
Dmitry Rog <dimanius851@gmail.com>
2018-07-11 12:58:54 +0400

    Make organizations settings -> main settings filds ready for typing

--- src/scenes/Admin/AdminOrganisationSettings/components/ItemTabMainSettings/index.js
+++ src/scenes/Admin/AdminOrganisationSettings/components/ItemTabMainSettings/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 import css from "utils/css"
+
+import { compose, withStateHandlers } from "recompose"
+
 // components
 import PanelSettings from "../PanelSettings"
 import FormSettings from "../FormSettings"
@@ -22,54 +25,131 @@ import InputAddress from "base_components/InputAddress"
  * @see PanelContactDetails
  * -------------------------------------
  */
-const ItemTabMainSettings = () => (
+const ItemTabMainSettings = ({
+  organizationName,
+  displayName,
+  organizationDescription,
+  logo,
+  displayLogoInsteadOfDisplay,
+  contactAddress,
+
+  // TODO: use these fields
+  contactAddressCountry,
+  contactAddressState,
+  contactAddressZipCode,
+
+  website,
+  email,
+  telephone,
+  updateVal
+}) => (
   <FormSettings>
-    <PanelGeneralInformation />
-    <PanelContactDetails />
+    <PanelGeneralInformation
+      organizationName={organizationName}
+      displayName={displayName}
+      organizationDescription={organizationDescription}
+      logo={logo}
+      displayLogoInsteadOfDisplay={displayLogoInsteadOfDisplay}
+      updateVal={updateVal}
+    />
+    <PanelContactDetails
+      contactAddress={contactAddress}
+      website={website}
+      email={email}
+      telephone={telephone}
+      updateVal={updateVal}
+    />
     <ButtonSettings>Update</ButtonSettings>
   </FormSettings>
 )
 
-export default ItemTabMainSettings
+export default compose(
+  withStateHandlers(
+    {
+      organizationName: "",
+      displayName: "",
+      organizationDescription: "",
+      logo: null,
+      displayLogoInsteadOfDisplay: false,
+      contactAddress: "",
+      contactAddressCountry: "",
+      contactAddressState: "",
+      contactAddressZipCode: "",
+      website: "",
+      email: "",
+      telephone: ""
+    },
+    {
+      updateVal: props => (e, selector) => {
+        const value = e.target ? e.target.value : e
 
-/**
- * -------------------------------------
- * PanelGeneralInformation
- * @see ItemTabMainSettings
- * -------------------------------------
- */
-const PanelGeneralInformation = () => (
-  <PanelSettings title="General Information" paddingBottom="1.6em">
-    <FormGroup>
-      <Label>Organisation Name &#42;</Label>
-      <Text name="crossView" placeholder="CrossView" />
-    </FormGroup>
-    <FormGroup>
-      <Label>Display Name &#42;</Label>
-      <Text name="name" />
-    </FormGroup>
-    <FormGroup>
-      <Label>Description of Organisation</Label>
-      <TextArea
-        name="description"
-        row="4"
-        placeholder="Communication and culture is focused on those who want to serve in their local community."
-      />
-    </FormGroup>
-    <FormGroup>
-      <Label>Logo</Label>
-      <ContainerFlex alignItems="center">
-        <ButtonUpload />
-        <HintTextLogo>Recommended size: 160 x 55 px</HintTextLogo>
-      </ContainerFlex>
-      <ContainerDisplayLogo alignItems="center">
-        <DisplayTextLogo>Display logo instead of display name</DisplayTextLogo>
-        <Switch name="logo" />
-      </ContainerDisplayLogo>
-    </FormGroup>
-  </PanelSettings>
-)
+        return {
+          ...props,
+          [selector]: value
+        }
+      }
+    }
+  )
+)(ItemTabMainSettings)
 
+const PanelGeneralInformation = ({
+  organizationName,
+  displayName,
+  organizationDescription,
+  logo,
+  displayLogoInsteadOfDisplay,
+  updateVal
+}) => {
+  return (
+    <PanelSettings title="General Information" paddingBottom="1.6em">
+      <FormGroup>
+        <Label>Organisation Name &#42;</Label>
+        <Text
+          onChange={e => updateVal(e, "organizationName")}
+          value={organizationName}
+          name="crossView"
+          placeholder="CrossView"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label>Display Name &#42;</Label>
+        <Text
+          onChange={e => updateVal(e, "displayName")}
+          value={displayName}
+          name="name"
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label>Description of Organisation</Label>
+        <TextArea
+          onChange={e => updateVal(e, "organizationDescription")}
+          value={organizationDescription}
+          name="description"
+          row="4"
+          placeholder="Communication and culture is focused on those who want to serve in their local community."
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label>Logo</Label>
+        <ContainerFlex alignItems="center">
+          <ButtonUpload />
+          <HintTextLogo>Recommended size: 160 x 55 px</HintTextLogo>
+        </ContainerFlex>
+        <ContainerDisplayLogo alignItems="center">
+          <DisplayTextLogo>
+            Display logo instead of display name
+          </DisplayTextLogo>
+          <Switch
+            value={displayLogoInsteadOfDisplay}
+            onChange={e =>
+              updateVal(e.target.checked, "displayLogoInsteadOfDisplay")
+            }
+          />
+        </ContainerDisplayLogo>
+      </FormGroup>
+    </PanelSettings>
+  )
+}
 /**
  * -------------------------------------
  * HintTextLogo component
@@ -108,24 +188,50 @@ const DisplayTextLogo = css`
  * @see ItemTabMainSettings
  * -------------------------------------
  */
-const PanelContactDetails = () => (
+const PanelContactDetails = ({
+  contactAddress,
+  website,
+  email,
+  telephone,
+  updateVal
+}) => (
   <PanelSettings title="Contact Details" borderBottom="0" paddingBottom="0">
     <FormGroup>
       <Label>Address &#42;</Label>
-      <Text name="address" placeholder="Connect: Level 1" />
+      <Text
+        value={contactAddress}
+        onChange={e => updateVal(e, "contactAddress")}
+        name="address"
+        placeholder="Connect: Level 1"
+      />
     </FormGroup>
     <InputAddress />
     <FormGroup>
       <Label>Website &#42;</Label>
-      <Text name="website" placeholder="crossview.com.au" />
+      <Text
+        value={website}
+        onChange={e => updateVal(e, "website")}
+        name="website"
+        placeholder="crossview.com.au"
+      />
     </FormGroup>
     <FormGroup>
       <Label>Email &#42;</Label>
-      <Text name="email" placeholder="crossview.com.au" />
+      <Text
+        value={email}
+        onChange={e => updateVal(e, "email")}
+        name="email"
+        placeholder="crossview.com.au"
+      />
     </FormGroup>
     <FormGroup>
       <Label>Telephone</Label>
-      <Text name="telePhone" placeholder="000000" />
+      <Text
+        value={telephone}
+        onChange={e => updateVal(e, "telephone")}
+        name="telePhone"
+        placeholder="000000"
+      />
     </FormGroup>
   </PanelSettings>
 )
