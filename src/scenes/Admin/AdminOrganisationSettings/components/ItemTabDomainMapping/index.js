a72840fad088b295c035ad03a675b3d0416e81fb
Dmitry Rog <dimanius851@gmail.com>
2018-07-11 22:09:29 +0400

    Refactored organizations settings -> domain mapping

--- src/scenes/Admin/AdminOrganisationSettings/components/ItemTabDomainMapping/index.js
+++ src/scenes/Admin/AdminOrganisationSettings/components/ItemTabDomainMapping/index.js
@@ -1,14 +1,23 @@
 import React from "react"
-import styled from "styled-components"
-// components
-import Form from "./components/Form"
-import Panel from "./components/Panel"
-import Button from "./components/Button"
-import Wrapper from "./components/Wrapper"
+
 import { FormGroup, Label, Text, Switch } from "base_components/RootForm"
 import ContainerFlex from "base_components/ContainerFlex"
 
-//
+import {
+  SubdomainText,
+  Postfix,
+  StatusIcon,
+  StatusTitle,
+  Status,
+  GrayContainer,
+  ContainerDisplayLogo,
+  DisplayTextLogo,
+  Panel,
+  Button,
+  Form,
+  Wrapper
+} from "./elements"
+
 const PanelWebAddress = () => (
   <Panel title="Web Address" paddingBottom="1.6em">
     <FormGroup>
@@ -21,15 +30,6 @@ const PanelWebAddress = () => (
   </Panel>
 )
 
-const SubdomainText = Text.extend`
-  max-width: 356px;
-`
-
-const Postfix = styled.div`
-  margin-left: 12px;
-`
-
-//
 const PanelDomainMapping = () => (
   <Panel title="Domain Mapping" borderBottom="0" paddingBottom="0">
     <ContainerDisplayLogo alignItems="center">
@@ -52,41 +52,8 @@ const PanelDomainMapping = () => (
   </Panel>
 )
 
-const StatusIcon = styled.div`
-  background-color: ${({ isConnected: x }) => (x ? "#2FE330" : "#D0021B")};
-  width: 11px;
-  height: 11px;
-  border-radius: 50%;
-  margin-left: 3px;
-  margin-right: 11px;
-`
-
-const StatusTitle = styled.span`
-  font-size: 14px;
-`
-
-const Status = styled.span`
-  font-size: 11px;
-`
-
-const GrayContainer = styled.div`
-  background-color: #f3f3f3;
-  padding: 22px;
-  margin-bottom: 10px;
-`
-
-const ContainerDisplayLogo = styled(ContainerFlex)`
-  margin-top: 0.6em;
-  margin-bottom: 2.6em;
-`
-
-const DisplayTextLogo = styled.span`
-  margin-right: 2em;
-`
-
-//
-const Descriptions = styled(({ className }) => (
-  <Panel className={className}>
+const Descriptions = () => (
+  <Panel>
     <p>Instructions for linking to custom domain</p>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -113,18 +80,8 @@ const Descriptions = styled(({ className }) => (
       est laborum.
     </p>
   </Panel>
-))`
-  padding-top: 200px;
-  border-bottom: none;
+)
 
-  @media screen and (max-width: 767px) {
-    padding-top: 30px;
-    padding-left: 2%;
-    padding-right: 2%;
-  }
-`
-
-//
 const ItemTabDomainMapping = () => (
   <Wrapper>
     <Form>
