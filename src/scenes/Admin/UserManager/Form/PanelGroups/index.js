245955298164debca709b13543be81076ad585d6
Dmitry Rog <dimanius851@gmail.com>
2018-07-14 18:39:48 +0400

    Added "Add group" modal

--- src/scenes/Admin/UserManager/Form/PanelGroups/index.js
+++ src/scenes/Admin/UserManager/Form/PanelGroups/index.js
@@ -1,22 +1,28 @@
 import React from "react"
 
+import Modal from "../GroupModal"
+
 import { Root, List } from "./elements"
 import { ButtonIcon } from "base_components/RootButton"
 import IconAdd from "react-icons/lib/md/add"
 
-const AddButton = () => (
-  <ButtonIcon light lightBorder>
-    <IconAdd /> Add group
-  </ButtonIcon>
-)
+import modal from "hoc/modal"
 
-const AccountContainer = () => (
-  <Root title="Groups" AdditionalTitle={AddButton}>
+const AccountContainer = ({ onOpen, onClose, isOpen }) => (
+  <Root
+    title="Groups"
+    AdditionalTitle={() => (
+      <ButtonIcon onClick={onOpen} light lightBorder>
+        <IconAdd /> Add group
+      </ButtonIcon>
+    )}
+  >
     <List>
       <div>group1</div>
       <div>group2</div>
     </List>
+    <Modal isOpen={isOpen} handleClose={onClose} />
   </Root>
 )
 
-export default AccountContainer
+export default modal(AccountContainer)
