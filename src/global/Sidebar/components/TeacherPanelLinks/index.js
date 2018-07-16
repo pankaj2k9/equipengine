4279b32cffa7c1f60a9592b4b4d01d0c1d9b1c08
Dmitry Rog <dimanius851@gmail.com>
2018-07-16 20:25:23 +0400

    Added popover to menu list item

--- src/global/Sidebar/components/TeacherPanelLinks/index.js
+++ src/global/Sidebar/components/TeacherPanelLinks/index.js
@@ -1,16 +1,24 @@
 import React from "react"
+
+import Popover from "base_components/Popover"
+
 import Menu from "../Menu"
 import Link from "../Link"
+
+import { LinkWithPopover } from "../elements"
 
 // this is for teacher panel links
 const TeacherPanelLinks = () => (
   <div className="Sidebar__body__otherPages">
     <Menu>
       <Link text="Groups Activiy" url="/secure/teacher/groups-activity" />
-      <Link
-        text="Submissions Activity"
-        url="/secure/teacher/submissions-activity"
-      />
+      <LinkWithPopover>
+        <Link
+          text="Submissions Activity"
+          url="/secure/teacher/submissions-activity"
+        />
+        <Popover dataCount={10} />
+      </LinkWithPopover>
       <Link text="User Manager" url="/secure/teacher/user-manager" />
     </Menu>
   </div>
---
+++ src/global/Sidebar/components/elements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const LinkWithPopover = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  & > *:last-child {
+    position: relative;
+    top: -4px;
+  }
+`
