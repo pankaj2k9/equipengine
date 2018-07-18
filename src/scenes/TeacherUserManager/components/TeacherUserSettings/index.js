ede10d89e0323a937e62f4c5e2b5fb27d1cd5a66
Dmitry Rog <dimanius851@gmail.com>
2018-07-18 22:48:18 +0400

    Added user settings tab

--- src/scenes/TeacherUserManager/components/TeacherUserSettings/index.js
+++ src/scenes/TeacherUserManager/components/TeacherUserSettings/index.js
@@ -6,15 +6,51 @@ import DragDrop from "base_components/DragDrop"
 import { MainInnerContainer } from "base_components/Main"
 import { Tab } from "react-bootstrap"
 import Tabs from "base_components/Tabs"
+import UserDetails from "base_components/UserDetails"
 // child components.
 import ItemTeacherUserCourses from "./components/ItemTeacherUserCourses"
 
-// for teacher user settings.
-const ItemTeacherUserSettings = () => (
+import { Header } from "./elements"
+import form from "hoc/form"
+import { compose, withHandlers, flattenProp } from "recompose"
+
+const ItemTeacherUserSettings = ({
+  firstName,
+  changeFirstName,
+  lastName,
+  changeLastName,
+  email,
+  changeEmail
+}) => (
   <div style={{ fontSize: "1.09rem", padding: "0px 20px", color: "#111" }}>
-    User Details
+    <Header>User Details</Header>
+    <UserDetails
+      firstName={firstName}
+      changeFirstName={changeFirstName}
+      lastName={lastName}
+      changeLastName={changeLastName}
+      email={email}
+      changeEmail={changeEmail}
+    />
   </div>
 )
+
+const onChange = key => ({ onChange }) => ({ target: { value } }) =>
+  onChange(value, key)
+
+const EnhancedItemTeacherUserSettings = compose(
+  form({
+    firstName: "",
+    lastName: "",
+    email: ""
+  }),
+  withHandlers({
+    changeFirstName: onChange("firstName"),
+    changeLastName: onChange("lastName"),
+    changeEmail: onChange("email")
+  }),
+  flattenProp("fields")
+)(ItemTeacherUserSettings)
 
 const TeacherUserSettings = styled(({ className }) => (
   <MainInnerContainer className={className}>
@@ -25,7 +61,7 @@ const TeacherUserSettings = styled(({ className }) => (
         </DragDrop>
       </Tab>
       <Tab eventKey={2} title="User Settings">
-        <ItemTeacherUserSettings />
+        <EnhancedItemTeacherUserSettings />
       </Tab>
     </Tabs>
   </MainInnerContainer>
