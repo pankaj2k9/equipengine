27b872d7a32d2067bc63a22f0e1e42ba0a474430
pavel pauliushchyk <p.pavliushchik@aisnovations.com>
2018-08-02 09:00:18 +0300

    hoc/modal was applied

--- src/scenes/Course/Thread/index.js
+++ src/scenes/Course/Thread/index.js
@@ -1,48 +1,22 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
+import React from "react"
 import IconPlus from "react-icons/lib/fa/plus"
 
 import { ButtonIcon } from "base_components/RootButton"
+import modal from "hoc/modal"
 
 import CreateThreadModal from "../CreateThreadModal"
 
-class Thread extends Component {
-  constructor() {
-    super()
+const Thread = ({ isOpen, onClose, onOpen }) => (
+  <div>
+    <ButtonIcon secondary onClick={onOpen}>
+      New Thread
+      <i>
+        <IconPlus />
+      </i>
+    </ButtonIcon>
 
-    this.state = {
-      showModal: false
-    }
-  }
+    <CreateThreadModal isOpen={isOpen} onClose={onClose} onSubmit={() => {}} />
+  </div>
+)
 
-  close = () => {
-    this.setState({ showModal: false })
-  }
-
-  open = () => {
-    this.setState({ showModal: true })
-  }
-
-  render() {
-    const { showModal } = this.state
-
-    return (
-      <div>
-        <ButtonIcon secondary onClick={this.open}>
-          New Thread
-          <i>
-            <IconPlus />
-          </i>
-        </ButtonIcon>
-
-        <CreateThreadModal
-          isOpen={showModal}
-          onClose={this.close}
-          onSubmit={() => {}}
-        />
-      </div>
-    )
-  }
-}
-
-export default Thread
+export default modal(Thread)
