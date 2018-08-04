535ada91789c7f19f52bfe3229afc3d3f9674f61
pavel pauliushchyk <p.pavliushchik@aisnovations.com>
2018-08-04 20:53:03 +0300

    MessageForm was applied

--- src/scenes/Dashboard/components/NoticeboardDashboard/components/BodyDashboard/index.js
+++ src/scenes/Dashboard/components/NoticeboardDashboard/components/BodyDashboard/index.js
@@ -1,24 +1,15 @@
 import React from "react"
 
-import Form, { TextArea } from "base_components/RootForm"
-import Button from "base_components/RootButton"
+import MessageForm from "base_components/MessageForm"
 
 import ListNotice from "./ListNotice"
-
-import { AttachmentButton, ButtonsContainer, Body } from "./elements"
+import { Body, FormContainer } from "./elements"
 
 const BodyDashboard = () => (
   <Body>
-    <Form>
-      <TextArea
-        row={2}
-        name="noticeboard"
-        placeholder="Write on the noticeboard..."
-      />
-      <ButtonsContainer>
-        <AttachmentButton /> <Button>Submit</Button>
-      </ButtonsContainer>
-    </Form>
+    <FormContainer>
+      <MessageForm />
+    </FormContainer>
     <ListNotice />
   </Body>
 )
