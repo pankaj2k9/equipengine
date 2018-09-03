2d661588042880cf6d8102f46e90b6e873fbc029
Dan Shatilo <mail.for.work.dan@gmail.com>
2018-09-03 06:01:04 +0000

    Merged in feature-files (pull request #117)
    Feature files
    
    *  try to add sort && infinity scroll
    
    * fix some bugs.
    
    * add correct infiniti load with search
    
    * fix conflict
    
    * fix some bugs.
    
    * add correct infiniti load with search
    
    Approved-by: Alex Soroka <lehasvv2009@gmail.com>

--- src/scenes/Files/thunks.js
+++ src/scenes/Files/thunks.js
@@ -2,21 +2,50 @@ import * as API from "services/API"
 import { actions as filesActions } from "./ducks"
 import { ATTACHMENTABLE_GROUP_TYPE } from "services/constants"
 
-export const fetchFiles = ({ attachmentable_id }) => {
+export const fetchFiles = ({ attachmentable_id, term }) => {
   return async dispatch => {
-    dispatch(filesActions.fetchGroupFilesRequest())
+    dispatch(
+      filesActions.fetchGroupFilesRequest({
+        term
+      })
+    )
 
     try {
       const { attachments, meta } = await API.fetchAttachments({
         attachmentable_id,
-        attachmentable_type: ATTACHMENTABLE_GROUP_TYPE
+        attachmentable_type: ATTACHMENTABLE_GROUP_TYPE,
+        term
       })
 
       return dispatch(
-        filesActions.fetchGroupFilesSuccess({ attachments, meta })
+        filesActions.fetchGroupFilesSuccess({ attachments, meta, term })
       )
     } catch (error) {
       return dispatch(filesActions.fetchGroupFilesError())
+    }
+  }
+}
+
+export const fetchMoreGroupFiles = ({
+  attachmentable_id,
+  term,
+  pagination
+}) => {
+  return async dispatch => {
+    dispatch(filesActions.fetchMoreGroupFilesRequest())
+
+    try {
+      const { attachments, meta } = await API.fetchAttachments({
+        attachmentable_id,
+        attachmentable_type: ATTACHMENTABLE_GROUP_TYPE,
+        pagination,
+        term
+      })
+      return dispatch(
+        filesActions.fetchMoreGroupFilesSuccess({ attachments, meta })
+      )
+    } catch (error) {
+      return dispatch(filesActions.fetchMoreGroupFilesError())
     }
   }
 }
