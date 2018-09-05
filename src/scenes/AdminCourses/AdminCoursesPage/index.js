8de58653c09be5e50cd3c9551217bf2b5f60d61d
Subrat <06.subrata@gmail.com>
2018-09-05 18:21:43 +0530

    child routes removed from admin courses page

--- src/scenes/AdminCourses/AdminCoursesPage/index.js
+++ src/scenes/AdminCourses/AdminCoursesPage/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
 import styled from "styled-components"
 
 import CoursesContainer from "scenes/AdminCoursesContainer"
 import BorderedTitle from "base_components/BorderedTitle"
-import ErrorBoundary from "base_components/ErrorBoundary"
 import CoursesActionBar from "../CoursesActionBar"
 import CoursesTable from "../CoursesTable"
-import features from "features"
 
 const StyledCoursesActionBar = styled(CoursesActionBar)`
   padding: 16px;
@@ -19,34 +16,11 @@ const StyledCoursesTable = styled(CoursesTable)`
 `
 
 const AdminCoursesPage = props => {
-  const { match } = props
   return (
     <CoursesContainer>
       <BorderedTitle title="Course Creator" />
-
-      <div>
-        <Switch>
-          <Route
-            exact
-            path={`${match.url}`}
-            render={props => (
-              <ErrorBoundary errMsg="Something went wrong in displaying the course page.">
-                <StyledCoursesActionBar />
-                <StyledCoursesTable {...props} />
-              </ErrorBoundary>
-            )}
-          />
-          <Route
-            exact
-            path={`${match.url}/:courseId`}
-            render={props => (
-              <ErrorBoundary errMsg="Something went wrong in displaying the course page.">
-                <features.adminCourse.pages.Course {...props} />
-              </ErrorBoundary>
-            )}
-          />
-        </Switch>
-      </div>
+      <StyledCoursesActionBar />
+      <StyledCoursesTable {...props} />
     </CoursesContainer>
   )
 }
