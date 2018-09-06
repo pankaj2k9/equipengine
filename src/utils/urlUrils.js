3b1e3c212e3fd45cafe982ae1e5e63facc5f610e
Subrat <06.subrata@gmail.com>
2018-09-06 23:02:53 +0530

    blanket group id extraction from any group route

--- src/utils/urlUrils.js
+++ src/utils/urlUrils.js
@@ -1,5 +1,5 @@
 // regexp extract :groupId from /secure/groups/1 like urls
 export const extractGroupId = url => {
-  const match = /\/secure\/admin\/groups\/([\d\w-_]+).*/g.exec(url)
+  const match = /[\w\s]+\/groups\/([\d\w-_]+).*/g.exec(url)
   return match && match.length > 1 ? match[1] : null
 }
